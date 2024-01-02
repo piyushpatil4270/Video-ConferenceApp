83ce667f16d40fc259a7403f8dae01edbf34f3c0
Piyush <piyushpatil4270@gmail.com>
2024-01-02 22:51:39 +0530

    handled events of calling and accepting calls

--- server/index.js
+++ server/index.js
@@ -18,5 +18,14 @@ io.on("connection",(socket)=>{
     socket.join(room)
     io.to(socket.id).emit("joinroom",data)
     })
+
+
+    socket.on("user:call",({to,offer})=>{
+        io.to(to).emit("incoming:call",{from: socket.id, offer})
+    })
+
+    socket.on("call:accepted",({to, ans})=>{
+        io.to(to).emit("call:accepted",{from: socket.id, ans})
+    })
 })
 
