ef7d501f3c1a5fce2b41534ddceea0dbffab390d
Piyush <piyushpatil4270@gmail.com>
2024-01-02 22:19:27 +0530

    initialized peer services

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,31 +1,53 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { useSocket } from '../Context/SocketProvider'
-
+import React, {
+  startTransition,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import { useSocket } from "../Context/SocketProvider";
+import ReactPlayer from "react-player";
 const Room = () => {
-    const socket = useSocket()
-    const [remoteSocketId,setRemoteSocketId]=useState(null )
-    const handleUserJoined=useCallback(({email,id})=>{
-        console.log(`Email ${email} joined room ${id}`)
-        setRemoteSocketId(id)
-    },[])
+  const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState();
+  const handleUserJoined = useCallback(({ email, id }) => {
+    console.log(`Email ${email} joined room ${id}`);
+    setRemoteSocketId(id);
+  }, []);
 
-    const handleCallUser=()=>{
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    setMyStream(stream);
+  }, []);
 
-    }
-    
-    useEffect(()=>{
-        socket.on("userjoined",handleUserJoined)
-        return ()=>{
-            socket.off("userjoined",handleUserJoined)
-        }
-    },[socket,handleUserJoined])
+  useEffect(() => {
+    socket.on("userjoined", handleUserJoined);
+    return () => {
+      socket.off("userjoined", handleUserJoined);
+    };
+  }, [socket, handleUserJoined]);
   return (
     <div>
       <h1>Room Page</h1>
-      <h4>{remoteSocketId?"You are connected":"No one is in the room"}</h4>
-      {remoteSocketId && <button onClick={()=>handleCallUser()}>Call</button>}
+      <h4>{remoteSocketId ? "You are connected" : "No one is in the room"}</h4>
+      {remoteSocketId && (!myStream && <button onClick={() => handleCallUser()}>Call</button>)}
+      {myStream && (
+        <>
+        <h4>My Stream</h4>
+        <ReactPlayer
+          playing
+          muted
+          width="200px"
+          height="100px"
+          url={myStream}
+        />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Room
+export default Room;
